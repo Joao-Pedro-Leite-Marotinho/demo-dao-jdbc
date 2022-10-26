b7c248b8a70cb5bc1f3b94c14e6d8e7af95b8337
JP <jplmarotinho22@gmail.com>
2022-10-26 09:15:36 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import model.dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+	
+	//when instancing a new SellerDao, Program will call for DaoFactory
+	//when using this implementation, the Program only calls the interface
+	//dependency injection without expliciting the implementation
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC();
+	}
+	
+}
