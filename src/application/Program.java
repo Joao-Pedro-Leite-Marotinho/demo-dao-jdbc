56d0018ffeebd41e4ff9659da18a139cb42f4f69
JP <jplmarotinho22@gmail.com>
2022-10-31 09:19:17 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -43,6 +43,13 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "Greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: seller update ===");
+		seller = sellerDao.finById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Updated.");
+		
 	}
 
 }
